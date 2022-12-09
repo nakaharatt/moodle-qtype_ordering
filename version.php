d82ccd0bac0ce148fa28515cdc405dc05822ff54
Takahiro NAKAHARA <nakahara@3strings.co.jp>
2022-12-09 17:03:25 +0900

    Update version.php
    Version情報を本家に合わせて修正

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ $plugin->cron      = 0;
 $plugin->component = 'qtype_ordering';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2015051100; // Moodle 2.9.
-$plugin->version   = 2022050102; //2021091404;
-$plugin->release   = '2022-05-1 (01)';
+$plugin->version   = 2022070804; //customize
+$plugin->release   = '2022-07-08 (07)';
