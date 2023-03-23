c3e71042a0f914681c64c22858484dc53a1016ce
Takahiro NAKAHARA <nakahara@3strings.co.jp>
2023-03-23 13:25:02 +0900

    Fix missed newversion number

--- db/upgrade.php
+++ db/upgrade.php
@@ -283,7 +283,7 @@ function xmldb_qtype_ordering_upgrade($oldversion) {
     }
 
     //@3strings
-    $newversion = '2022070807';
+    $newversion = '2022070804';
     if ($oldversion < $newversion) {
         $table = new xmldb_table('qtype_ordering_options');
 
